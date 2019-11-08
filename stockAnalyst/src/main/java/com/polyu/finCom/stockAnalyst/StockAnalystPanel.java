607e4550c15f2a35f5a0cc54c7d520dded034a16
MicYip <348196155@qq.com>
2019-11-09 01:22:52 +0800

    Yulong first commit,加入登录系统

--- stockAnalyst/src/main/java/com/polyu/finCom/stockAnalyst/StockAnalystPanel.java
+++ stockAnalyst/src/main/java/com/polyu/finCom/stockAnalyst/StockAnalystPanel.java
@@ -11,9 +11,15 @@ import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 public class StockAnalystPanel {
 
+    private static JFrame jf;
+    // 获取txt文件变量
+    private static File[] files;
 
     /**
      * Main Start
@@ -23,7 +29,8 @@ public class StockAnalystPanel {
     }
 
     private StockAnalystPanel() {
-        JFrame jf = new JFrame("Stock Analyser");
+        //JFrame jf = new JFrame("Stock Analyser");
+        jf = new JFrame("Stock Analyser");
         jf.setSize(1200, 900);
         jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jf.setLocationRelativeTo(null);
@@ -39,7 +46,7 @@ public class StockAnalystPanel {
         // 创建选项卡面板
         final JTabbedPane tabbedPane = new JTabbedPane();
 
-        tabbedPane.addTab("File", createTextPanel("File"));
+        tabbedPane.addTab("File", createloadPanel());
 
         tabbedPane.addTab("Stock", createTextPanel("Stock"));
 
@@ -72,6 +79,61 @@ public class StockAnalystPanel {
         panel.add(label);
 
         return panel;
+    }
+
+    // File标签中的面板
+    private static JComponent createloadPanel(){
+        // 创建中间容器
+        // 创建面板
+        JPanel panel = new JPanel();
+
+        // 创建文本区域，用于显示相关信息
+        final JTextArea msgTextArea = new JTextArea(10,30);
+        msgTextArea.setLineWrap(true);
+
+        // 创建一个button
+        JButton open_button = new JButton("打开按钮");
+        // Button的监听
+        open_button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // 打开文件，选择文件保存后文件存在变量files中
+                showFileOpenDialog(jf,msgTextArea);
+            }
+        });
+        panel.add(open_button);
+        panel.add(msgTextArea);
+
+        return panel;
+    }
+
+    private static void showFileOpenDialog(Component parent, JTextArea msgTextArea){
+
+        // 创建一个默认的文件选取器
+        JFileChooser fileChooser = new JFileChooser();
+
+        // 设置默认显示的文件夹为当前文件夹
+        fileChooser.setCurrentDirectory(new File("."));
+
+        // 设置文件选择的模式（文件和文件夹均可选）
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        // 设置是否允许多选
+        fileChooser.setMultiSelectionEnabled(true);
+
+        // 打开文件选择框（线程将被阻塞，知道选择框被关闭）
+        int result = fileChooser.showOpenDialog(parent);
+
+        if (result == JFileChooser.APPROVE_OPTION){
+            // 如果点击了“确定”，则获取选择的文件路径
+            //File file = fileChooser.getSelectedFile();
+
+            // 如果选择多个文件，则通过下面方法获取选择的所有文件
+            files = fileChooser.getSelectedFiles();
+            for (int i = 0; i < files.length; i++) {
+                msgTextArea.append("打开文件： " + files[i].getAbsolutePath() + "\n\n");
+            }
+
+        }
+
     }
 
 }
